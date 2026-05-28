6bd9ec8dbdee6f769632c5ebd0cdc81786992d18
Sarvarbek2026 <sarvaryoldoshib@gmail.com>
2026-05-28 15:09:44 +0500

    Update config.py

--- config.py
+++ config.py
@@ -1,11 +1,13 @@
 import os
 
-BOT_TOKEN = os.environ.get("BOT_TOKEN", "8877332711:AAHfiIQif4LWtHPlaPHXuvK3AZvFSB1qD8w")
-RAPID_API_KEY = os.environ.get("RAPID_API_KEY", "5e826a9b24msheafeaf09a41a683p12ab3fjsncd1c35b1f313")
+BOT_TOKEN = os.environ.get("BOT_TOKEN")
+RAPID_API_KEY = os.environ.get("RAPID_API_KEY")
 BOT_USERNAME = os.environ.get("BOT_USERNAME", "smart_pdf_video_bot")
 
 VIDEO_SITES = [
-    'youtube.com', 'youtu.be', 
-    'instagram.com', 'tiktok.com', 'vm.tiktok.com', 'pinterest.com', 
-    'pin.it', 'facebook.com', 'fb.watch', 'twitter.com', 'x.com'
+    'youtube.com', 'youtu.be',
+    'instagram.com', 'tiktok.com', 'vm.tiktok.com',
+    'pinterest.com', 'pin.it',
+    'facebook.com', 'fb.watch',
+    'twitter.com', 'x.com'
 ]
