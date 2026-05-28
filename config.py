2cf231643e91c41565799680c0e4e882ace230b9
Sarvarbek2026 <sarvaryoldoshib@gmail.com>
2026-05-28 14:04:46 +0500

    Update config.py

--- config.py
+++ config.py
@@ -1,1 +1,11 @@
+import os
 
+BOT_TOKEN = os.environ.get("BOT_TOKEN", "8927416207:AAFA2t6g7Ka5SMKfBLeaeGfcW8v8StI08eg")
+RAPID_API_KEY = os.environ.get("RAPID_API_KEY", "5e826a9b24msheafeaf09a41a683p12ab3fjsncd1c35b1f313")
+BOT_USERNAME = os.environ.get("BOT_USERNAME", "sarvar_image_bot")
+
+VIDEO_SITES = [
+    'youtube.com', 'youtu.be', 
+    'instagram.com', 'tiktok.com', 'vm.tiktok.com', 'pinterest.com', 
+    'pin.it', 'facebook.com', 'fb.watch', 'twitter.com', 'x.com'
+]
